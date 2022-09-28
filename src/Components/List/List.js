80f424b052a0557396ae2ace26a45ebe7d4b86b4
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-09-29 05:09:26 +0600

    finished design of list components

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -13,6 +13,46 @@ const List = () => {
                     <span>Dhaka, Bangladesh</span>
                 </div>
             </div>
+            <div className="measurement_card list_card">
+                <div className="weight">
+                    <h2>75<sub>kg</sub> </h2>
+                    <p>Weight</p>
+                </div>
+                <div className="height">
+                    <h2>6.5</h2>
+                    <p>Height</p>
+                </div>
+                <div className="age">
+                    <h2>27<sub>yrs</sub> </h2>
+                    <p>Age</p>
+                </div>
+            </div>
+            <div className="add_break">
+                <h3>Add A Break</h3>
+                <div className="list_card">
+                    <div className="break_time">
+                        <p>10s</p>
+                        <p>20s</p>
+                        <p>30s</p>
+                        <p>40s</p>
+                    </div>
+                </div>
+            </div>
+            <div className="exercise_details">
+                <h3>Exercise Details</h3>
+                <div className="exercise_time list_card">
+                    <p>Exercise time</p>
+                    <p>200 seconds</p>
+                </div>
+                <div className="break_time list_card">
+                    <p>Break time</p>
+                    <p>15 seconds</p>
+                </div>
+            </div>
+
+            <div className="completed_btn">
+                <button>Activity Completed</button>
+            </div>
         </div>
     );
 };
