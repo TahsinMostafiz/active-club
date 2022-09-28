1f2559fad2083e00f8dd49d09a321e405977cf62
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-09-29 02:09:26 +0600

    added home and activities components

---
+++ src/Components/Activities/Activities.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Activities = (props) => {
+    console.log(props.activity);
+    const {name, img, time, details, for_age} = props.activity
+    return (
+        <div className='activity_div'>
+            <div className="card">
+                <img src={img} alt="" />
+                <div className="card_body">
+                    <h2 className="title">{name}</h2>
+                    <p className="text">{details}</p>
+                    <h4>For Age : {for_age}</h4>
+                    <h5>Time required : {time}</h5>
+                </div>
+                <div className="card_footer">
+                    <button>Add to list</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Activities;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Activities from '../Activities/Activities';
+import './Home.css';
+
+const Home = () => {
+    const [gymActivities, setGymActivities] = useState([])
+
+    useEffect(() => {
+        fetch('blackfit_fitness_club_data.json')
+        .then(res => res.json())
+        .then(data => setGymActivities(data))
+    },[]);
+    return (
+        <div className='home'>
+            <div className="left_container">
+                <header>
+                    <h2>Blackfit Fitness Club</h2>
+                </header>
+                <main>
+                    <h3>Select today's exercise</h3>
+                    <div className="activities_div">
+                        {
+                            gymActivities.map(activity => <Activities key={activity.id} activity={activity}></Activities>)
+                        }
+                    </div>
+                </main>
+            </div>
+            <div className="right_container">
+                <h2>Right Container</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
